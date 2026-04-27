91f885c83ee31ede3596642685c8180ea679c46a
Lee Wei Quan <leewq-wm23@student.tarc.edu.my>
2026-04-27 22:04:36 +0800

    Add AppGradientBackground widget
    Implement a gradient background widget for the app.

--- lib/widgets/app_gradient_background.dart
+++ lib/widgets/app_gradient_background.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+class AppGradientBackground extends StatelessWidget {
+  final Widget child;
+
+  const AppGradientBackground({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xFF667EEA),
+            Color(0xFF764BA2),
+          ],
+        ),
+      ),
+      child: SafeArea(child: child),
+    );
+  }
+}
